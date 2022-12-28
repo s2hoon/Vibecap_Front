80e4124b87d44746fb75eccd496c05b2711f14a9
ddddabi <ii887742@gmail.com>
2022-12-28 19:11:45 +0900

    VibeMain layout 구성

---
+++ app/src/main/java/com/example/vibecap/VibeMainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.vibecap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.vibecap.databinding.ActivityVibeMainBinding
+
+class VibeMainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityVibeMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityVibeMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
