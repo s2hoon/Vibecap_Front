8a959758fccc8a3cda217346a20824195e2f4b61
suhoon <jobak4200nim@nate.com>
2023-01-03 21:35:52 +0900

    circle wheel 임시구현

--- app/src/main/java/com/example/vibecap/MainActivity.kt
+++ app/src/main/java/com/example/vibecap/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.example.vibecap
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.graphics.drawable.Drawable
+import android.graphics.drawable.ShapeDrawable
+import android.graphics.drawable.shapes.OvalShape
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.lukedeighton.wheelview.WheelView
+import com.lukedeighton.wheelview.adapter.WheelAdapter
 
 class MainActivity : AppCompatActivity() {
+    private var wheelView: WheelView? = null
+    private val colors = arrayOf("#fd5308", "#fd5308", "#fd5308", "#fd5308", "#fd5308", "#fd5308","#fd5308","#fd5308","#fd5308","#fd5308","#fd5308","#fd5308")
+    var size = 12
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        wheelView = findViewById<View>(R.id.wheelview) as WheelView
+        wheelView!!.setWheelItemCount(size)
+        val shapeDrawables = arrayOfNulls<ShapeDrawable>(size)
+        for (i in 0 until size) {
+            shapeDrawables[i] = ShapeDrawable(OvalShape())
+            shapeDrawables[i]!!.paint.color = Color.parseColor(colors[i])
+        }
+        wheelView!!.adapter = object : WheelAdapter {
+            override fun getDrawable(position: Int): Drawable {
+                return shapeDrawables[position]!!
+            }
+
+            override fun getCount(): Int {
+                return size
+            }
+        }
+        wheelView!!.onWheelItemClickListener =
+            WheelView.OnWheelItemClickListener { parent, position, isSelected ->
+                Toast.makeText(
+                    this@MainActivity,
+                    "You Clicked",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
     }
 }
